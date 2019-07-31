ae0e01fe4f66f12b388016d88f30f052f2e35f8c
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-07-30 22:15:06 -0700

    pseudocoded schedule.js

---
+++ schedule.js
@@ -0,0 +1,11 @@
+// var input = something with how you record the input from the form
+    // if any of the inputs are empty
+        // alert/tell user that they need to fill the form
+    // else if they are all full
+        // record the input
+
+// when input is recorded
+    // calculate next arrival time
+    // calculate how many minutes away via firebase
+    // output all data to row
+        // append every new train
